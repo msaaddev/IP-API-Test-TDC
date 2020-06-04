cd3640b9c9327962b3c37b09e4a5f71ac92e6ab5
msaaddev <mrsaadirfan@gmail.com>
2020-06-04 19:36:20 +0500

    📦 NEW: api testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 (() => {
     axios
         .get(
-            'http://api.ipapi.com/182.185.176.244?access_key=ACCESS_TOKEN&format=1'
+            'http://api.ipapi.com/182.185.176.244?access_key=ACCESS_TOKEN_HERE&format=1'
         )
         .then(({ data }) => console.log(data).catch((err) => console.log(err)));
 })();
