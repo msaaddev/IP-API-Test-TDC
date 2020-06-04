ac9441a2d791cd87ebbc63a089c7f00aad850c76
msaaddev <mrsaadirfan@gmail.com>
2020-06-04 20:06:15 +0500

    🐛 FIX: promise error

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // importing axios
 const axios = require('axios');
 
-(() => {
-    axios
-        .get(
-            'http://api.ipapi.com/182.185.176.244?access_key=ACCESS_TOKEN_HERE&format=1'
-        )
-        .then(({ data }) => console.log(data).catch((err) => console.log(err)));
+(async () => {
+    await axios
+        .get('http://api.ipapi.com/182.185.176.244?access_key=ACCESS_TOKEN_HERE&format=1')
+        .then(({ data }) => console.log(data))
+        .catch((err) => console.log(err));
 })();
