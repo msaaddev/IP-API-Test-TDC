8e1ff685040cad0d8cb1574cd4483e1da47bcbc8
msaaddev <mrsaadirfan@gmail.com>
2020-06-04 19:35:54 +0500

    📦 NEW:

---
+++ index.js
@@ -0,0 +1,10 @@
+// importing axios
+const axios = require('axios');
+
+(() => {
+    axios
+        .get(
+            'http://api.ipapi.com/182.185.176.244?access_key=ACCESS_TOKEN&format=1'
+        )
+        .then(({ data }) => console.log(data).catch((err) => console.log(err)));
+})();
